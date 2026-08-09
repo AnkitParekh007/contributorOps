4aa3b504921a65606765d1265b49c73d12fe4b92
Ankit Parekh <155077196+AnkitParekh007@users.noreply.github.com>
2026-08-09 06:05:29 +0200

    initialize optional analytics and adoption styles

--- apps/site/src/main.tsx
+++ apps/site/src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { initAnalytics } from "./lib/analytics";
 import "./styles.css";
 import "./engagement.css";
 import "./growth.css";
 import "./authority.css";
 import "./distribution.css";
+import "./adoption.css";
+
+initAnalytics();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
